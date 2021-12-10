74f2a5674762ca2d61525b8555dee8d9f37727e1
Jônatas Santos <jonatas.santos@zapier.com>
2021-12-10 17:00:26 -0300

    Day 10 - some refactor

--- 2021/10/main.go
+++ 2021/10/main.go
@@ -7,48 +7,38 @@ import (
 	"strings"
 )
 
+type W = struct {
+	c      string
+	points int
+}
+
 func first() {
 	input := base.GetLines()
 
-	chunkMap := map[string]struct {
-		open   string
-		points int
-	}{
+	cMap := map[string]W{
 		")": {"(", 3},
 		"]": {"[", 57},
 		"}": {"{", 1197},
 		">": {"<", 25137},
 	}
 
-	errors := []string{}
-
-	for _, line := range input {
-		stack := []string{}
-
-		for _, r := range line {
-			s := string(r)
-
-			if strings.ContainsAny(s, "([{<") {
-				stack = append(stack, s)
-			} else {
-				pop := stack[len(stack)-1]
-
-				if pop == chunkMap[s].open {
-					stack = stack[:len(stack)-1]
-				} else {
-					errors = append(errors, s)
-					break
-				}
-			}
-		}
-	}
+	errors := findErrors(input, cMap)
 
 	score := 0
 	for _, err := range errors {
-		score += chunkMap[err].points
+		score += cMap[err].points
 	}
 
 	fmt.Println("Score", score)
+}
+
+func findErrors(input []string, cMap map[string]W) []string {
+	errors := []string{}
+
+	for _, line := range input {
+		errors, _, _ = findError(line, cMap, errors)
+	}
+	return errors
 }
 
 func second() {
@@ -69,67 +59,73 @@ func second() {
 		"<": {">", 4},
 	}
 
-	errors := []string{}
-	completitions := [][]string{}
+	var completions [][]string
 
 	for _, line := range input {
-		stack := []string{}
-		err := false
-
-		for _, r := range line {
-			s := string(r)
-
-			if strings.ContainsAny(s, "([{<") {
-				stack = append(stack, s)
-			} else {
-				pop := stack[len(stack)-1]
-
-				if pop == cMap[s].c {
-					stack = stack[:len(stack)-1]
-				} else {
-					err = true
-					errors = append(errors, s)
-					break
-				}
-			}
-		}
+		_, stack, err := findError(line, cMap, []string{})
 
 		if !err {
-			closing := []string{}
-			completition := []string{}
-			for {
-				if len(stack) == 0 {
-					break
-				}
-
-				pop := stack[len(stack)-1]
-				stack = stack[:len(stack)-1]
-
-				if strings.ContainsAny(pop, ")]}>") {
-					closing = append(closing, pop)
-				} else {
-					if len(closing) > 1 {
-						last_closing := closing[len(closing)-1]
-						if cMap[pop].c != last_closing {
-							closing = closing[:len(closing)-1]
-						}
-					} else {
-						completition = append(completition, cMap[pop].c)
-					}
-				}
-
-			}
-			completitions = append(completitions, completition)
+			completions = findCompletions(stack, cMap, completions)
 		}
 	}
 
-	// calculate scores
-	scores := calculateScores(completitions, cMap)
-
-	// look for the winner
+	scores := calculateScores(completions, cMap)
 	winner := findTheWinner(scores)
 
 	fmt.Println("Score 2:", winner)
+}
+
+func findError(line string, cMap map[string]W, errors []string) ([]string, []string, bool) {
+	stack := []string{}
+	err := false
+
+	for _, r := range line {
+		s := string(r)
+
+		if strings.ContainsAny(s, "([{<") {
+			stack = append(stack, s)
+		} else {
+			pop := stack[len(stack)-1]
+
+			if pop == cMap[s].c {
+				stack = stack[:len(stack)-1]
+			} else {
+				err = true
+				errors = append(errors, s)
+				break
+			}
+		}
+	}
+	return errors, stack, err
+}
+
+func findCompletions(stack []string, cMap map[string]W, completions [][]string) [][]string {
+	closing := []string{}
+	completion := []string{}
+	for {
+		if len(stack) == 0 {
+			break
+		}
+
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if strings.ContainsAny(pop, ")]}>") {
+			closing = append(closing, pop)
+		} else {
+			if len(closing) > 1 {
+				last_closing := closing[len(closing)-1]
+				if cMap[pop].c != last_closing {
+					closing = closing[:len(closing)-1]
+				}
+			} else {
+				completion = append(completion, cMap[pop].c)
+			}
+		}
+
+	}
+	completions = append(completions, completion)
+	return completions
 }
 
 func findTheWinner(scores []int) int {
@@ -138,12 +134,9 @@ func findTheWinner(scores []int) int {
 	return winner
 }
 
-func calculateScores(completitions [][]string, cMap map[string]struct {
-	c      string
-	points int
-}) []int {
+func calculateScores(completions [][]string, cMap map[string]W) []int {
 	scores := []int{}
-	for _, c := range completitions {
+	for _, c := range completions {
 		score := 0
 		for _, s := range c {
 			score = score*5 + cMap[s].points
