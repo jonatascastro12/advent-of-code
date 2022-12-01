f6b5fb3375058546caf74c3dd4ea8bb2dd582ca6
Jônatas Santos <jonatascastro12@gmail.com>
2022-12-01 13:34:59 -0300

    Day 01

---
+++ 2022/src/main/kotlin/Day_01/Main.kt
@@ -0,0 +1,27 @@
+package Day_01
+
+import java.io.File
+import java.nio.file.Paths
+
+fun main() {
+    val path = Paths.get("").toAbsolutePath().toString()
+    val fileName = "$path/src/main/kotlin/Day_01/input.txt"
+    val lines = File(fileName).readLines()
+    val elfCalories = mutableListOf<Int>()
+
+    var lastElf = 0
+    lines.forEach {
+        if (it != "") lastElf += it.toInt()
+        else {
+            elfCalories.add(lastElf)
+            lastElf = 0
+        }
+    }
+    println("First Part: " + elfCalories.sortedDescending()[0])
+    println(
+        "Second Part: " + (
+                elfCalories.sortedDescending()[0] +
+                        elfCalories.sortedDescending()[1] +
+                        elfCalories.sortedDescending()[2])
+    )
+}
